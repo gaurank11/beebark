80bcb7dea94c1cd8652f572dd079f2083c78c4b4
Nikhil Singh <95328684+nikhil-singh1@users.noreply.github.com>
2025-08-02 17:00:39 +0530

    Update aboutus.jsx

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "/Agency logo-Photoroom.png"
+import logo from "/Agency logo-Photoroom1.png"
 import { useNavigate } from 'react-router-dom'; 
 
 const AboutUs = () => {
